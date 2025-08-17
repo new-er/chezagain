7af3229d1baadb7a903674a20945cdff093e3ccf
Dominik Schraml <dominik.schraml97@gmail.com>
2025-08-17 02:48:11 +0200

    add diffs

--- git/git.go
+++ git/git.go
@@ -6,17 +6,20 @@ import (
 
 
 func AddAll() error {
+	println("Adding all changes to git...")
 	cmd := exec.Command("git", "add", ".")
 	return cmd.Run()
 }
 
 func Commit(message string) error {
+	println("Committing changes with message:", message)
 	cmd := exec.Command("git", "commit", "-m", message)
 	return cmd.Run()
 }
 
 
 func Push() error {
+	println("Pushing changes to remote repository...")
 	cmd := exec.Command("git", "push")
 	return cmd.Run()
 }
